005e7e51f5de1add0f1d0d326a70bd5a8e6609e2
VincentAulnay <48682002+VincentAulnay@users.noreply.github.com>
2020-02-19 11:08:24 +0100

    Update multi.py

--- multi.py
+++ multi.py
@@ -2,7 +2,7 @@ import urllib.request
 
 
 #code = 'https://raw.githubusercontent.com/VincentAulnay/test_private/master/BB_FROMWEB.py?token=AubUEoiuCQqSlrT4C6RQRvSjk2q_hy8bks5cj6TSwA%3D%3D'
-code = 'https://raw.githubusercontent.com/VincentAulnay/test2/master/BB_FROMWEB.py'
+code = 'https://raw.githubusercontent.com/VincentAulnay/ODROID_scraper/master/print.py'
 response = urllib.request.urlopen(code)
 data = response.read()
 
